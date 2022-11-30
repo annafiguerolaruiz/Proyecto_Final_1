3664cc411841270685ff593bf9cc4b4a9c7593a1
annafiguerolaruiz <91455277+annafiguerolaruiz@users.noreply.github.com>
2022-11-30 19:24:05 +0100

    AF

---
+++ Assets/Scripts/Menus.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menus : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SceneGame()
+    {
+        SceneManager.LoadScene("AF_Game");
+    }
+     
+    public void SceneOptions()
+    {
+        SceneManager.LoadScene("AF_Options");
+    }
+    public void SceneScores()
+    {
+        SceneManager.LoadScene("AF_Scores");
+    }
+    public void SceneMenu()
+    {
+        SceneManager.LoadScene("AF_MainMenu");
+    }
+
+}
